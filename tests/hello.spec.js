e5e886fcaa7c0480741cd3deaf4e73e7e9e8d3c7
Nurettin Topal <nurettintopal@gmail.com>
2020-01-14 23:58:44 +0300

    adds mocha as a test framwork

---
+++ tests/hello.spec.js
@@ -0,0 +1,9 @@
+import assert from 'assert';
+
+describe('tests', function () {
+    describe('hello tests', function () {
+        it('test one', function () {
+            assert.equal(1, 1);
+        });
+    });
+});
